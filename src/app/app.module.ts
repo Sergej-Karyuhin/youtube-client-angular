0ebb4fed362441765c1014809f6e9b507f90b1e2
Serge <karyuhin.sergej@gmail.com>
2020-09-04 14:36:05 +0300

    fix: connecting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { SearchItemComponent } from './components/search-item/search-item.component';
-import { FilterComponent } from './components/filter/filter.component';
+import { SearchItemComponent } from './components/search-results/search-item/search-item.component';
+import { FilterComponent } from './components/header/filter/filter.component';
 
 @NgModule({
   declarations: [
