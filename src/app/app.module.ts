763184873c63915ec23cf6aa51c9b2c01ef18773
Serge <karyuhin.sergej@gmail.com>
2020-09-04 10:54:37 +0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchItemComponent } from './components/search-item/search-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent
   ],
   imports: [
     BrowserModule
